2b6d3fe847acaf75f75995076532cab09820a064
Sunil Verma <sunilverma00027@gmail.com>
2025-11-30 22:51:12 +0530

    Fix AWS EB deployment: add xgboost to requirements, fix port binding in application.py

--- application.py
+++ application.py
@@ -41,4 +41,5 @@ def predict_datapoint():
 
 # Run locally only
 if __name__ == "__main__":
-    application.run(debug=True)
+    port = int(os.environ.get('PORT', 8080))
+    application.run(host='0.0.0.0', port=port, debug=True)
